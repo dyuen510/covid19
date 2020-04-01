effba0fe4d95c67cd1c8b824dcdabe06a668e992
David <davidyuen0305@gmail.com>
2020-03-31 21:04:41 -0700

    added a style.css sheet to the project.

--- server.js
+++ server.js
@@ -4,13 +4,14 @@ $.ajax({
     url: queryURL,
     method: 'GET'
 }).then(function (response) {
-    for (let i = 0; i<= response.length; i++) {
+    for (let i = 1; i <= response.length - 1; i++) {
+        console.log(response[i])
         if(response[i].Province == 'California'){
             // console.log(response[i].Province);
             // console.log(response[i].Date);
             // console.log(response[i].Cases);
             // console.log(response[i].Status);
-            let province = $('<p>').text('Province: ' + response[i].Province);
+            let province = $('<p>').text('State: ' + response[i].Province);
             let date = $('<p>').text('Date: ' + response[i].Date);
             let cases = $('<p>').text('Number of Cases Confirmed: ' + response[i].Cases);
             let status = $('<p>').text('Status: ' + response[i].Status);
