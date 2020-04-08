0ea778b7fe1d0101606451076091cf5727e47987
David <davidyuen0305@gmail.com>
2020-04-07 22:17:12 -0700

    displayed some titles and a snippet on project.

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ $.ajax({
     url: queryURL,
     method: 'GET'
 }).then(function (response) {
-    for ( let i = 0; i <= response.length; i++) {
+    for (let i = 0; i <= response.length; i++) {
         console.log(response[i]);
         let state = $('<h3>').text(response[i].state);
         let cases = $('<p>').text('Total Cases: ' + response[i].cases);
@@ -16,16 +16,26 @@ $.ajax({
         $('.content').append(display);
         console.log(state)
     }
-        
+
 })
 
 const newsQueryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?q=covid19&api-key=pqTAhnco7pVR9FUqC09qgrNtbfwV1CZH";
 
-  $.ajax({
+$.ajax({
     url: newsQueryURL,
     method: 'GET'
-  }).then(function (response) {
+}).then(function (response) {
     console.log(response);
-  })
+    for (var i = 0; i < 7; i++) {
+        console.log(response);
+
+        var p = $('<h5>').text('Title: ' + response.response.docs[i].headline.main);
+        var c = $('<p class="des-item">').text(response.response.docs[i].snippet);
+        var newsList = $('<div class="res-news">').append(p, c);
+
+        $('.news').prepend(newsList);
+    }
+
+})
 
 
