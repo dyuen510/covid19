76cdb53ef088846892712844f8c66ccef76d4b87
David <davidyuen0305@gmail.com>
2021-10-23 17:22:25 -0700

    made some changes to the site. Also added a new div with the style display:flex, align-content and flex-wrap

--- server.js
+++ server.js
@@ -19,27 +19,5 @@ $.ajax({
 
 })
 
-const newsQueryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?q=covid19&api-key=pqTAhnco7pVR9FUqC09qgrNtbfwV1CZH";
-
-$.ajax({
-    url: newsQueryURL,
-    method: 'GET'
-}).then(function (response) {
-    // console.log(response);
-    for (var i = 0; i < 10; i++) {
-        // console.log(response);
-
-        var p = $('<h3>').text('Title: ' + response.response.docs[i].headline.main);
-        var c = $('<p class="des-item">').text(response.response.docs[i].snippet);
-        var dBtn = $(`<a class="btn-link" target="_blank"><button> For more Go to link </button></a>`).text("For more Go to link");
-        dBtn.attr("href", response.response.docs[i].web_url);
-
-        var newsList = $('<div id ="news">').append(p, c, dBtn);
-        $('.news').prepend(newsList);
-
-    }
-
-
-})
 
 
